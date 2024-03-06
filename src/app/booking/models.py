2946613d7249571f1bce091ee218ce0e168e119d
samugd17 <samugd17@gmail.com>
2024-03-06 17:06:09 +0000

    booking model edited  #7

--- src/app/booking/models.py
+++ src/app/booking/models.py
@@ -6,5 +6,5 @@ class Booking(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
     price = models.DecimalField(max_digits=4, decimal_places=2)
     duration = models.DurationField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE) #issue: comprobar si ManyToMany o ForeignKey
-    product = models.ForeignKey(Product, on_delete=models.CASCADE) #issue: comprobar si ManyToMany o ForeignKey
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, primary_key=True)
