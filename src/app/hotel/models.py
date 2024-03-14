2c116d8ac37e178cbe9d680f0cbf6e095154501d
AbianGustavo <71705574+AbianGustavo@users.noreply.github.com>
2024-03-14 10:03:02 +0000

    #7 Created passwords for users and fixes

--- src/app/hotel/models.py
+++ src/app/hotel/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import random
+import string
 
 
 class Hotel(models.Model):
@@ -13,19 +15,25 @@ class Hotel(models.Model):
     code = models.CharField(max_length=16, editable=False)
     # site_map
 
-    @property
     def save(self, *args, **kwargs):
-        city_code = self.city[:2].upper()
-        hotel_code = ''.join(word[:2].upper() for word in self.name.split())
-        self.code = f"{city_code}-{hotel_code}"
+        if not self.pk:
+            city_code = self.city[:2].upper()
+            hotel_code = ''.join(word[:2].upper() for word in self.name.split())
+            self.code = f"{city_code}-{hotel_code}"
         super().save(*args, **kwargs)
 
 
 class User(models.Model):
     username = models.CharField(max_length=20, unique=True, null=False)
+    password = models.charField(max_length=16, editable=False)
     email = models.EmailField(max_length=50, null=False)
     num_guest = models.SmallIntegerField(max_length=2, null=False)
     hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE)
 
-    def create_password(self):  # issue: crear contraseña aleatoria
-        pass
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            chars = string.ascii_letters + string.digits + string.punctuation
+            password_random = ''.join(random.choice(chars) for _ in range(16))
+            self.password = make_password(password_random)
+        super().save(*args, **kwargs)
+    
