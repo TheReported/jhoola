e51e66db51add4287a8e531851e6d2384e154cf8
samugd17 <samugd17@gmail.com>
2024-04-11 17:18:04 +0100

    fixed bookings url

--- src/app/back-end/booking/urls.py
+++ src/app/back-end/booking/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 app_name = 'booking'
 urlpatterns = [
+    path('<username>/book/', views.booking_view, name='client_book'),
     path('<username>/bookings/', views.booking_list, name='booking_list'),
     path('<username>/bookings/<booking_id>/pdf/', views.booking_pdf, name='booking_pdf'),
+
 ]
--- src/app/back-end/users/urls.py
+++ src/app/back-end/users/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 
 from . import views
 
-from booking.views import booking_view 
 
 app_name = 'users'
 urlpatterns = [
@@ -11,6 +10,6 @@ urlpatterns = [
     path('manager/', views.manager_dashboard, name='manager_dashboard'),
     path('manager/users/', views.users_manager_view, name='manager_users'),
     # path('<username>/bookings/', views.client_dashboard, name='client_dashboard'),
-    path('<username>/book/', booking_view, name='client_book'),
+
 
 ]
