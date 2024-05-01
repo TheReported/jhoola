32d8c23591463a163d32d3542979ff1353913313
samugd17 <samugd17@gmail.com>
2024-05-01 18:14:02 +0100

    removed unused slug field on product model #32

--- src/app/back-end/product/admin.py
+++ src/app/back-end/product/admin.py
@@ -5,6 +5,5 @@ from .models import Product
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name', 'slug', 'price', 'avatar', 'status', 'hotel']
+    list_display = ['name', 'price', 'status', 'hotel']
     raw_id_fields = ['hotel']
-    prepopulated_fields = {'slug': ('name',)}
