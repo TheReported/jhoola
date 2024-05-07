d76e77aaa93c26154e2c43f9d64dae8927d254b9
TheReported <diegopca12@gmail.com>
2024-05-07 18:08:05 +0100

    Fix errors on QR #35

--- src/app/back-end/users/decorators.py
+++ src/app/back-end/users/decorators.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse
 
+from hotel.models import Hotel
+
 from .models import Client
 
 
@@ -16,11 +18,13 @@ def client_required(func):
 
 def manager_required(func):
     def wrapper(request, *args, **kwargs):
-        client = get_object_or_404(Client, user=request.user)
+        selected_hotel = request.session.get('hotel_session_name')
+        hotel = Hotel.objects.get(name=selected_hotel)
+        if not request.user.is_authenticated:
+            return redirect("users_login")
+        client = get_object_or_404(Client, user=request.user, hotel=hotel)
         if client.user.groups.filter(name="HotelManagers").exists():
             return func(request, *args, **kwargs)
-        return redirect(
-            reverse("booking:booking_list", kwargs={'username': client.user.username})
-        )
+        return redirect(reverse("booking:booking_list", kwargs={'username': client.user.username}))
 
     return wrapper
