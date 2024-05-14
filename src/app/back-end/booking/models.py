213736b3581f787dc14e2084941de66c47944499
samugd17 <samugd17@gmail.com>
2024-05-14 23:00:48 +0100

    added personalized hotel timeslots

--- src/app/back-end/booking/models.py
+++ src/app/back-end/booking/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 from product.models import Product
 from users.models import Client
+from hotel.models import Hotel
 
 
 class PaidManager(models.Manager):
@@ -15,7 +16,17 @@ class Booking(models.Model):
         AFTERNOON = 'AFT', 'Afternoon'
         ALL_DAY = 'ALL', 'All Day'
 
-    user = models.ForeignKey(Client, on_delete=models.CASCADE, related_name="bookings")
+        @classmethod
+        def get_display_with_hours(cls, slot, morning_hours, afternoon_hours):
+            slot_display = dict(cls.choices)[slot]
+            if slot == cls.MORNING:
+                return f'{slot_display} ({morning_hours})'
+            elif slot == cls.AFTERNOON:
+                return f'{slot_display} ({afternoon_hours})'
+            else:
+                return slot_display
+
+    user = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='bookings')
     products = models.ManyToManyField(Product)
     timestamp = models.DateTimeField(auto_now_add=True, editable=False)
     price = models.DecimalField(max_digits=8, decimal_places=2)
@@ -28,3 +39,12 @@ class Booking(models.Model):
 
     def __str__(self):
         return f'{self.id}'
+
+    @property
+    def get_duration_display(self):
+        if self.duration == self.TimeSlots.MORNING:
+            return f'Morning ({Hotel.opening_morning_hours})'
+        if self.duration == self.TimeSlots.AFTERNOON:
+            return f'Afternoon ({Hotel.opening_afternoon_hours})'
+        else:
+            return 'All Day'
--- src/app/back-end/hotel/models.py
+++ src/app/back-end/hotel/models.py
@@ -1,4 +1,11 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+import re
+
+
+def validate_opening_hours(value):
+    if not re.match(r'^[0-1][0-9]:[0-5][0-9] - [0-2][0-9]:[0-5][0-9]$', value):
+        raise ValidationError("The format of opening hours must be 'hh:mm - hh:mm'")
 
 
 class Hotel(models.Model):
@@ -9,7 +16,20 @@ class Hotel(models.Model):
     email = models.EmailField(max_length=50)
     phone = models.CharField(max_length=12)
     code = models.CharField(max_length=16, editable=False, blank=True, null=True)
-    site_map = models.CharField(max_length=100, blank=True)
+    opening_morning_hours = models.CharField(
+        max_length=20,
+        validators=[validate_opening_hours],
+        default='09:00 - 14:00',
+        blank=False,
+        null=False,
+    )
+    opening_afternoon_hours = models.CharField(
+        max_length=20,
+        validators=[validate_opening_hours],
+        default='14:00 - 19:00',
+        blank=False,
+        null=False,
+    )
 
     def save(self, *args, **kwargs):
         if not self.pk:
